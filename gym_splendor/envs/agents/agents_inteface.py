0fa54c723e8a4bb7cb726f207fa96604b64cde09
LAPTOP-6I1KC8ML\nguye <annguyen@vis-laboratory.com>
2022-05-15 09:06:10 +0700

    no message

---
+++ gym_splendor/envs/agents/agents_inteface.py
@@ -0,0 +1,7 @@
+from gym_splendor.envs.agents import agent_3 as a1
+from gym_splendor.envs.agents import agent_3 as a2
+from gym_splendor.envs.agents import agent_3 as a3
+from gym_splendor.envs.agents import agent_3 as a4
+
+ListPlayer = [a1.Agent("Xuân"),a2.Agent("Hạ"),a3.Agent("Thu"),a4.Agent("Đông")]
+# 
---
+++ main.py
@@ -0,0 +1,44 @@
+import gym
+import gym_splendor
+import os
+import pandas as pd
+
+def check_winner(state):
+    name = ''
+    score_max = 14
+    player_win = None
+    if (state['Turn']+1)%4 == 0:
+        for player in list(state['Player']):
+            if player.score > score_max:
+                score_max = player.score 
+        if score_max > 14:
+            for player in list(state['Player']):
+                if player.score >= score_max:
+                    score_max = player.score 
+                    player_win = player
+                elif player.score == score_max:
+                    if len(player.card_open) < len(player_win.card_open):
+                        player_win = player
+    if player_win != None:
+        # pd.read_csv(f'State_tam_{player_win.name}.csv').assign(win = 1).to_csv(f'State_tam_{player_win.name}.csv', index = False)
+        return player_win.name, score_max, state['Turn']+1
+    else:
+        return "None"
+
+def main():
+    env = gym.make('gym_splendor-v0')
+    env.reset()
+    env.render()
+    while env.turn <500:
+        o,a,done,t = env.step(env.player[env.turn%env.amount_player].action(env.state))
+        env.render()
+        if env.turn % 50 == 1:
+            input()
+        if done == True:
+            break
+    state = env.state
+    print(check_winner(state))
+
+if __name__ == '__main__':
+    main()
+
