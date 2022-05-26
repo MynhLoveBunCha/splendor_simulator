6657febab54d071883416d39f623dce367fb4fd3
Minh Nhat Doan <doannhatminh253@gmail.com>
2022-05-26 16:22:21 +0700

    final commit

--- gym_splendor/envs/agents/agents_inteface.py
+++ gym_splendor/envs/agents/agents_inteface.py
@@ -1,7 +1,7 @@
 from gym_splendor.envs.agents import agent_1 as a1
 from gym_splendor.envs.agents import agent_nhat_minh as a2
 from gym_splendor.envs.agents import agent_1 as a3
-from gym_splendor.envs.agents import agent_1 as a4
+from gym_splendor.envs.agents import agent_4 as a4
 
 ListPlayer = [a1.Agent("lmao1"),a2.Agent("nhat_minh"),a3.Agent("lmao3"),a4.Agent("lmao4")]
 # 
